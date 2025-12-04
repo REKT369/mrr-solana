120975cef108f9c00cc4f4930d610988694048eb
REKT369 <REKT__@outlook.com>
2025-12-04 21:39:43 +1100

    Create constants.ts

---
+++ clients/ts-mrr/src/constants.ts
@@ -0,0 +1,8 @@
+import { PublicKey } from "@solana/web3.js";
+
+/**
+ * MRR program ID on Solana Devnet.
+ */
+export const MRR_PROGRAM_ID = new PublicKey(
+  "61cVB9Sj5dWWGk5fUfPCCvwpRtpMds2QW4Q5G7UqaDYV"
+);
